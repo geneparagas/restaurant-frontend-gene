a85bdfd85ecce1e73b43b15392c087e5743ba70e
gene <gene@qjt.sg>
2019-02-04 23:14:38 +0800

    Updates

---
+++ precache-manifest.96f8be58c11d528dba8043f5d783576a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ac6331e9836d82ed05b",
+    "url": "/restaurant-frontend-gene/static/js/runtime~main.9ac6331e.js"
+  },
+  {
+    "revision": "5a5f66facc8dc1fce135",
+    "url": "/restaurant-frontend-gene/static/js/main.5a5f66fa.chunk.js"
+  },
+  {
+    "revision": "ad0438c04175c70677f9",
+    "url": "/restaurant-frontend-gene/static/js/1.ad0438c0.chunk.js"
+  },
+  {
+    "revision": "5a5f66facc8dc1fce135",
+    "url": "/restaurant-frontend-gene/static/css/main.0939966d.chunk.css"
+  },
+  {
+    "revision": "e0e455f62de6fcee8e717fd7d33a4bd9",
+    "url": "/restaurant-frontend-gene/index.html"
+  }
+];
